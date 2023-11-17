10af40417af42a659adcb9613046355cac937887
hugo B <hb.hugobabin@gmail.com>
2023-11-17 09:01:55 +0100

    add tree strcu + save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import MindNode from "./components/MindNode";
+
+class Tree{
+  constructor(){
+    this.root = new Node(1,"test");
+  }
+
+  /* Tree data strcute example :
+  *  
+  * Tree :
+  * 
+  *   RootNode [
+  *     ChildNode1 [
+  *       ChildNode1.1 [
+  *         ChildNode1.1.1[]
+  *         ChildNode1.1.2[]
+  *       ]
+  *       ChildNode1.2 []
+  *     ]
+  *     
+  *     ChildNode2 [
+  *       ChildNode2.1[]
+  *       ChildNode2.2[
+  *         ChildNode2.2.1[]
+  *       ]
+  *     ]
+  *   ]
+  */
+
+  traverse(callback){
+    const arr = [this.root];
+    //loop through array
+    while(arr.length){
+      //we take into consideration the first element, the 
+      const node = arr.shift();
+      //we now push the childens sub-tree into the array for next iterations
+      arr.push(node.children)
+      callback(node)
+    }
+  }
+
+
+}
+
+class Node {
+  construcor(id, data) {
+    this.id = id;
+    this.data = data;
+    this.children = [];
+    this.width = 1;
+  }
+
+  add(id, data){
+    this.children.push(new Node(id, data));
+    this.width = this.children.length; // update the witdth of this node
+  }
+
+  remove(id){
+    this.children = this.children.filter((node) => {
+      return node.id !== id;
+    });
+    this.width = this.children.length; // update the witdth of this node
+  }
+}
 
 function App() {
+  //initialize tree
+  const [tree, setTree] = useState(new Tree())
+  console.log(tree.root)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MindNode id="1" content={"test"}></MindNode>
     </div>
   );
 }
---
+++ src/components/MindNode.js
@@ -0,0 +1,9 @@
+function MindNode({id, content}) {
+  return (
+      <div className="MindNode">
+          <p>{content}</p>
+      </div>
+  );
+}
+
+export default MindNode;
